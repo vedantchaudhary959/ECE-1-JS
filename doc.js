106d2355e100e195f4db7104e5ae2b08c2a6fef8
vedantchaudhary959 <vedantchaudhary959@gmail.com>
2026-03-17 09:35:34 +0530

    Add files via upload
    new

---
+++ doc.js
@@ -0,0 +1,11 @@
+//let count;
+// document.write("Starting loop"+"<br/>");
+// for (count=0 ; count<10; count++){ doucument .write ("Current count:"+count +"<br/>");
+//}
+//document .write ("Loop stopped")
+
+var person2= new object();
+person2.name= "Vedant";
+person2.age ="19";
+person2.isMarried = false;
+person2.address= {};
